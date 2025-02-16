05d9cdab9eeea930ac9e5bf6db5b7843115deed0
E1dis <gustp1234567@naver.com>
2025-02-16 22:55:55 +0900

    NEW::LoadingSystem
    임시 로비 씬 추가
    씬 이동 시 로딩 시스템 추가

---
+++ Assets/Script/Lobby, Scene/RotateEffect.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace UnityNote
+{
+    public class RotateEffect : MonoBehaviour
+    {
+        [SerializeField]
+        private float rotateSpeed = 100;
+        [SerializeField]
+        private bool isPlay = true;
+
+        private void Update()
+        {
+            if (!isPlay) return;
+
+            transform.Rotate(Vector3.forward, rotateSpeed);
+        }
+    }
+}
