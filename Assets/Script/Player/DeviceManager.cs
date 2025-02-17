dbafdcaeb5a0b5c0111b57e93d839f1df4ce5391
E1dis <gustp1234567@naver.com>
2025-02-17 15:10:28 +0900

    NEW::NewDevice
    디바이스 UI 변경
    인벤토리 UI 변경

--- Assets/Script/Player/DeviceManager.cs
+++ Assets/Script/Player/DeviceManager.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class DeviceManager : MonoBehaviour
 {
     public GameObject uiPanel; // UI 패널
+    [SerializeField]
+    private GameObject inventoryPanel; // 인벤토리 패널
+    [SerializeField]
+    private GameObject equipmentPanel; // 장비 패널
 
     void Update()
     {
@@ -15,6 +19,9 @@ public class DeviceManager : MonoBehaviour
             uiPanel.SetActive(true);
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
+            // 처음 열었을 때 인벤토리 활성화
+            inventoryPanel.SetActive(true);
+            equipmentPanel.SetActive(true);
         }
 
         // Esc 키로 UI 비활성화
@@ -23,6 +30,8 @@ public class DeviceManager : MonoBehaviour
             uiPanel.SetActive(false);
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
+            inventoryPanel.SetActive(false);
+            equipmentPanel.SetActive(false);
         }
     }
 }
---
+++ Assets/Script/Player/Inventory/ButtonImageChanger.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonImageChanger : MonoBehaviour
+{
+    [System.Serializable]
+    public class UIButton
+    {
+        public Button button;
+        public Image buttonImage;
+        public Sprite defaultSprite;
+        public Sprite selectedSprite;
+    }
+
+    public UIButton[] uiButtons; // 4개의 버튼 리스트
+
+    void Start()
+    {
+        // 버튼에 클릭 이벤트 추가
+        foreach (UIButton uiButton in uiButtons)
+        {
+            uiButton.button.onClick.AddListener(() => OnButtonClicked(uiButton));
+        }
+    }
+
+    void OnButtonClicked(UIButton clickedButton)
+    {
+        foreach (UIButton uiButton in uiButtons)
+        {
+            // 선택한 버튼이면 UI 활성화 & 스프라이트 변경
+            bool isSelected = (uiButton == clickedButton);
+            uiButton.buttonImage.sprite = isSelected ? uiButton.selectedSprite : uiButton.defaultSprite;
+        }
+    }
+}
+
