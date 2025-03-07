316c8be46786a12ebb90e07ec480b02891bed562
Danny <danielhjpark@gmail.com>
2025-03-07 12:52:13 +0900

    FIX::SKYBox 수정
    시간에 따라 절차적으로 스카이박스가 변경되게 변경

--- Assets/Script/SkyBox/WorldTimeManager.cs
+++ Assets/Script/SkyBox/WorldTimeManager.cs
@@ -6,74 +6,56 @@ public class WorldTimeManager : MonoBehaviour
 {
     public GameTimeManager gameTimeManager;
     public Material[] skyboxes;
-    int skyIdx = 0;
+    private int skyIdx = 0;
+    private float blendValue = 0.0f;
 
     [SerializeField]
-    int currentHor = 0;
+    private int currentHour = 0;
 
     private void Update()
     {
-        if (currentHor != gameTimeManager.gameHours)
-        {
-            SunUpdate();
-        }
+        float gameHour = gameTimeManager.gameHours + (gameTimeManager.gameMinutes / 60.0f);
+        SunUpdate(gameHour);
     }
 
-    void SunUpdate()
+    void SunUpdate(float gameHour)
     {
-        currentHor = gameTimeManager.gameHours;
-        switch (currentHor)
+        if (currentHour != (int)gameHour)
         {
-            case 9:
-                StartCoroutine(NextSkyBox(skyboxes[0], 0));
-                break;
-
-            case 1:
-                Debug.Log("ㅋㅋ");
-                StartCoroutine(NextSkyBox(skyboxes[1], 1));
-                break;
-
-            case 18:
-
-                StartCoroutine(NextSkyBox(skyboxes[2], 2));
-                break;
-
-            case 22:
-                StartCoroutine(NextSkyBox(skyboxes[3], 3));
-                break;
+            currentHour = (int)gameHour;
+            switch (currentHour)
+            {
+                case 6:
+                    SetBlendSkyTexture(skyboxes[0]);
+                    skyIdx = 0;
+                    break;
+                case 12:
+                    SetBlendSkyTexture(skyboxes[1]);
+                    skyIdx = 1;
+                    break;
+                case 18:
+                    SetBlendSkyTexture(skyboxes[2]);
+                    skyIdx = 2;
+                    break;
+                case 24:
+                    SetBlendSkyTexture(skyboxes[3]);
+                    skyIdx = 3;
+                    break;
+            }
         }
+
+        blendValue = Mathf.Clamp01((gameHour % 6) / 6.0f);
+        RenderSettings.skybox.SetFloat("_Blend", blendValue);
     }
+
     void SetBlendSkyTexture(Material skybox)
     {
-        // 새 텍스처 생성
-
         string[] texParts = { "_FrontTex", "_BackTex", "_LeftTex", "_RightTex", "_UpTex", "_DownTex" };
 
-        // 각 면의 텍스처를 혼합
         foreach (string texPart in texParts)
         {
             RenderSettings.skybox.SetTexture(texPart, skyboxes[skyIdx].GetTexture(texPart));
             RenderSettings.skybox.SetTexture(texPart + "2", skybox.GetTexture(texPart));
         }
-
-    }
-
-    IEnumerator NextSkyBox(Material skybox, int idx)
-    {
-
-        float value = 0.0f;
-
-        // 각 면의 텍스처를 혼합
-        SetBlendSkyTexture(skybox);
-        while (value < 1)
-        {
-            yield return null;
-            value += Time.deltaTime * 0.1f;
-            value = Mathf.Clamp01(value);
-
-            RenderSettings.skybox.SetFloat("_Blend", value); // 혼합 비율 조절해 서서히 스카이 박스가 전환됨.
-        }
-
-        skyIdx = idx;
     }
 }
