66d13d954a72103513523fd0585c404385c78735
E1dis <gustp1234567@naver.com>
2025-03-16 15:59:34 +0900

    FIX::Spawn
    스폰 위치 변경

--- Assets/Script/Player/Spawn/PlayerSpawn.cs
+++ Assets/Script/Player/Spawn/PlayerSpawn.cs
@@ -16,6 +16,7 @@ public class PlayerSpawn : MonoBehaviour
         if (player)
         {
             player.transform.position = transform.position; // 씬 이동 후 스폰 포인트로 이동
+            player.transform.rotation = transform.rotation;
         }
     }
 
