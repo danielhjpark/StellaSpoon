a66762226fccd8cf1d151e61d499fcb01730a3a5
E1dis <gustp1234567@naver.com>
2025-03-16 20:58:58 +0900

    FIX::Spawn
    간혹 씬을 이동할 때 플레이어가 밑으로 사라지는 문제 수정

--- Assets/Script/Player/Spawn/PlayerSpawn.cs
+++ Assets/Script/Player/Spawn/PlayerSpawn.cs
@@ -2,27 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class PlayerSpawn : MonoBehaviour
 {
-    private void Awake()
+    private void OnEnable()
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        if (player)
-        {
-            player.transform.position = transform.position; // 씬 이동 후 스폰 포인트로 이동
-            player.transform.rotation = transform.rotation;
-        }
-    }
-
-    private void OnDestroy()
+    private void OnDisable()
     {
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        StartCoroutine(SetPlayerPosition());
+    }
+
+    private IEnumerator SetPlayerPosition()
+    {
+        yield return null; // 한 프레임 대기
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player)
+        {
+            CharacterController controller = player.GetComponent<CharacterController>();
+            if (controller)
+            {
+                controller.enabled = false; // 충돌 방지
+                player.transform.position = transform.position;
+                controller.enabled = true;
+            }
+            else
+            {
+                player.transform.position = transform.position;
+            }
+        }
+    }
 }
+
 
