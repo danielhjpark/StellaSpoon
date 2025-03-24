d156370d70ed4deebc2abc02f5bed2a49be505ee
E1dis <gustp1234567@naver.com>
2025-03-24 23:10:29 +0900

    FIX::DodgeLogic
    재장전을 하고 있을 때 회피 가능하게 수정 및 재장전 중 회피를 했을 때 재장전 취소

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -250,10 +250,15 @@ namespace StarterAssets
         private void Dodge()
         {
             // 가만히 있거나, 이미 닷지 중일 때는 닷지가 실행되지 않도록 함
-            if (_input.move == Vector2.zero || isDodge || dodgeCooldownActive || isReload || isAiming || isHit) return;
+            if (_input.move == Vector2.zero || isDodge || dodgeCooldownActive || isAiming || isHit) return;
 
             if (Grounded && _input.dodge)
             {
+                if(isReload)
+                {
+                    CancelReload();
+                }
+
                 StartCoroutine(DodgeCoroutine());
             }
         }
