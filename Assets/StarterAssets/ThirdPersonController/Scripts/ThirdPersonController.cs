bc6fb5332bb50f056978e630f232e90c1b0e7661
E1dis <gustp1234567@naver.com>
2025-05-04 22:36:22 +0900

    FIX::StoreInteraction
    상점을 열었을 때 플레이어가 움직이지 못하고 카메라 회전 못하게 수정

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -235,7 +235,7 @@ namespace StarterAssets
                 _hpBar.gameObject.SetActive(true);
             }
 
-            if (!DeviceManager.isDeactived || TreasureChest.openingChest || !WeaponChanger.isDeactived)
+            if (!DeviceManager.isDeactived || TreasureChest.openingChest || !WeaponChanger.isDeactived || StoreNPCManager.openingStoreUI)
             {
                 if (_hasAnimator)
                 {
@@ -258,7 +258,7 @@ namespace StarterAssets
 
         private void LateUpdate()
         {
-            if (!DeviceManager.isDeactived || TreasureChest.openingChest) return; // 비활성화 상태면 카메라 회전 막기 , 보물상자 오픈시 카메라 회전 막기
+            if (!DeviceManager.isDeactived || TreasureChest.openingChest || StoreNPCManager.openingStoreUI) return; // 비활성화 상태면 카메라 회전 막기 , 보물상자 오픈시 카메라 회전 막기
             CameraRotation();
         }
 
