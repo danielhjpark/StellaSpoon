24c792c7c8e162e8e0d718a06606e84e441f19fa
dolphin0118 <dlgksquf11118@naver.com>
2025-06-01 04:19:01 +0900

    Fix:: 레스토랑 상자에 아이템 추가

--- Assets/Script/Cook/RestaurantManager.cs
+++ Assets/Script/Cook/RestaurantManager.cs
@@ -5,33 +5,45 @@ using UnityEngine;
 public class RestaurantManager : MonoBehaviour
 {
     public static RestaurantManager instance;
-    public Item[] items;
+    [Header("메인재료")]
+    public Item[] mainIngredients;
+    [Header("서브재료")]
+    public Item[] subIngredients;
+    [Header("상자")]
     public Inventory chest1Inventory;
     public Inventory chest2Inventory;
-    public Inventory refrigeratorInventory;
-
+    public bool isInitialize;
+    
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
+            isInitialize = true;
         }
-        
+
     }
 
     private void Start()
     {
-        RefrigeratorAddIngredient();
+        AddIngredient();
         
     }
 
-    void RefrigeratorAddIngredient()
+    void AddIngredient()
     {
-        foreach (var item in items)
+        if (isInitialize) isInitialize = false;
+        else return;
+        foreach (var item in mainIngredients)
         {
-            chest1Inventory.AcquireItem(item, 10);
-            chest2Inventory.AcquireItem(item, 10);
+            chest1Inventory.AcquireItem(item, 1);
+            chest2Inventory.AcquireItem(item, 1);
+            RefrigeratorManager.instance.AddItem(item, 1);
+        }
+        foreach (var item in subIngredients)
+        {
             RefrigeratorManager.instance.AddItem(item, 10);
+            
         }
     }
 
