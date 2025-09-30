efb0a22e6c4874028b0ae0eaa0dda6c0c80fec7d
Danny <danielhjpark@gmail.com>
2025-09-30 19:12:31 +0900

    FIX::식당 스카이박스 변하는 버그 수정
    SkyBoxSetting.cs 내에서 스카이박스 고정으로 변경함과 동시에 행성정보 식당으로 수정

--- Assets/Script/SkyBox/SkyBoxSetting.cs
+++ Assets/Script/SkyBox/SkyBoxSetting.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static PlanetManager;
 
 public class SkyBoxSetting : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class SkyBoxSetting : MonoBehaviour
 
     void Start()
     {
+        PlanetManager.SetSelectedPlanet(PlanetType.Restaurant);
         RenderSettings.skybox = skyboxMaterial;
         // 만약 씬 전환 후 바로 반영 안 될 경우
         DynamicGI.UpdateEnvironment();
